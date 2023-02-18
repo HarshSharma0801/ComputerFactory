715f52082760cd04d7f67b36c4070476cde22be5
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-02-19 01:05:34 +0530

    Completing project

--- src/Product.java
+++ src/Product.java
@@ -9,13 +9,37 @@ public class Product {
         this.manufacturer = manufacturer;
     }
     class Monitor extends Product{
+        private int size;
+        private String resolution;
         public Monitor(String model, String manufacturer) {
             super(model, manufacturer);
         }
+
+        public Monitor(String model, String manufacturer, int size, String resolution) {
+            super(model, manufacturer);
+            this.size = size;
+            this.resolution = resolution;
+        }
+        public void drawPixelAt(int x , int y ,String color){
+            System.out.println(String.format("Drawing pixel at %d,%d inr color %s ",x,y,color));
+        }
     }
     class Motherboard extends Product{
+        private int ramSlots;
+        private int cardSlots;
+        private String bios;
         public Motherboard(String model, String manufacturer) {
             super(model, manufacturer);
+        }
+
+        public Motherboard(String model, String manufacturer, int ramSlots, int cardSlots, String bios) {
+            super(model, manufacturer);
+            this.ramSlots = ramSlots;
+            this.cardSlots = cardSlots;
+            this.bios = bios;
+        }
+        public void loadProgram(String programName){
+            System.out.println("program "+programName+" is now loading...");
         }
     }
     class ComputerCase extends Product{
